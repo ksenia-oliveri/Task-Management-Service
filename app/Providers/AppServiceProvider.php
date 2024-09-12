1cd4c3d0a82f15a7afde4bf2a28e4bbabcf08f57
ksjusha <ksenia081096@gmail.com>
2024-09-12 23:47:33 +0200

    cache and docker

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Events\UserCreated;
+use App\Listeners\UserCreatedListener;
+use Event;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Event::listen(
+            UserCreated::class,
+            UserCreatedListener::class,
+        );
     }
 }
--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -1,14 +1,17 @@
 <?php 
 
 namespace App\Services;
+use Cache;
 use Http;
+
 
 class UserService
 {
+   
     public function checkUserExists($userId)
     {
-        $response = Http::get("http://localhost:8001/api/users/{$userId}");
+        //$response = Http::get("http://localhost:8001/api/users/{$userId}");
+        return Cache::get('users:' . $userId);
 
-        return $response->status() == 200;
     }
 }
